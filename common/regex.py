b7ce7289746c4b30a66c61b68e9fac6a70b0c83c
pgseong <master@treasureisle.co>
2017-04-06 04:07:06 +0900

    username regex에 한글 추가

--- common/regex.py
+++ common/regex.py
@@ -4,7 +4,7 @@ __author__ = "Philgyu, Seong"
 __email__ = "phil@treasureisle.co"
 
 REGEX_EMAIL = r"[^@]+@[^@]+\.[^@]+"
-REGEX_USERNAME = r"^[a-zA-Z0-9_]{4,15}$"
+REGEX_USERNAME = r"^[a-zA-Z0-9ㄱ-ㅣ가-힣_]{4,15}$"
 # URL REGEX
 # https://mathiasbynens.be/demo/url-regex @imme_emosol 참고
 REGEX_URL = r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
